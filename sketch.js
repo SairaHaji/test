15825e52dcf17bccd3be1fc50dd1f2f606795aa6
SairaHaji <88104760+SairaHaji@users.noreply.github.com>
2021-07-29 12:17:31 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+  var hey = createSprite (200,200,40,40)
+  hey.shapeColor ="Gold"
+
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
